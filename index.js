d961a94910337b0b57d7fc3dce4fae187bba71e6
mjukicbraculj <mirjana.jukic-braculj@pixion.co>
2020-09-10 11:41:06 +0200

    add version to output

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ function setActionOutput(releaseResponse, { body }) {
       upload_url: uploadUrl,
       tag_name: tagName,
       name: name,
+      version: version,
     },
   } = releaseResponse
   if (releaseId && Number.isInteger(releaseId))
@@ -121,5 +122,6 @@ function setActionOutput(releaseResponse, { body }) {
   if (uploadUrl) core.setOutput('upload_url', uploadUrl)
   if (tagName) core.setOutput('tag_name', tagName)
   if (name) core.setOutput('name', name)
+  if (version) core.setOutput('version', version)
   core.setOutput('body', body)
 }
